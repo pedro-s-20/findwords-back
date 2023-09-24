546645482213f246d6778001176360cc0589b3d9
Pedro Sousa <pedro.programmer.20@gmail.com>
2023-09-24 14:52:25 -0300

    feat: add initial spring project

---
+++ findwords/src/main/java/ori/pedrosousa/findwords/FindwordsApplication.java
@@ -0,0 +1,13 @@
+package ori.pedrosousa.findwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindwordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindwordsApplication.class, args);
+	}
+
+}
